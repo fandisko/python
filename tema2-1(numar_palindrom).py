b9259587dab9b51d49278e55b2c7ef65856bac46
Liviu I <46314842+fandisko@users.noreply.github.com>
2020-02-05 23:01:58 +0200

    Add files via upload
    Tema 2-1 (numar palindrom)

---
+++ tema2-1(numar_palindrom).py
@@ -0,0 +1,30 @@
+x = int (input("Dati numarul x:"))
+s = 0
+p = 0
+i = 0
+sp = 0
+si = 0
+nrp = 0
+nrn = x
+while x != 0:
+    s = s + x % 10
+    if  x % 10 // 2 == 0 :
+        p = p + 1
+    else:
+        i = i + 1
+    if x % 10 % 2==0 :
+        sp = sp + x % 10
+    else:
+        si = si + x % 10
+    dig = x % 10
+    nrp = nrp * 10 + dig
+    x = x // 10
+print("Suma cifrelor este: ", s)
+print("Avem ", p, "numere pare!")
+print ("Avem ",i, "numere impare!")
+print("Suma numerelor pare este:", sp)
+print("Suma numerelor impare este:", si)
+if nrn == nrp :
+    print("Numarul este palindrom!")
+else:
+    print("Numarul nu este palindrom!")
